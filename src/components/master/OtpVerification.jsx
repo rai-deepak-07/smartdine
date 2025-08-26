a0c59474185cae193112373ad8981ad36494c820
Deepak Raikwar <raikwardeepak310@gmail.com>
2025-08-26 22:48:19 +0530

    raideepak07 26-08-25 22:48

---
+++ src/components/master/OtpVerification.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const OtpVerification = () => {
+  return (
+    <div>
+      <h2>OTP Verification</h2>
+    </div>
+  )
+}
+
+export default OtpVerification
--- src/components/user/UserRegister.jsx
+++ src/components/user/UserRegister.jsx
@@ -1,9 +1,99 @@
+import { Link, useNavigate } from 'react-router-dom';
 import React from 'react'
+import { useState } from 'react';
+import toast from 'react-hot-toast';
+import ApiService from '../../apiservice/ApiService';
 
 const UserRegister = () => {
+
+  const navigate = useNavigate(); 
+
+  const [formData, setFormData] = useState({
+    user_name: '',
+    user_email: '',
+    user_mobile_no: '',
+    user_password: '',
+    user_confirm_password: ''
+  });
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setFormData({ ...formData, [name]: value });
+  };
+
+  const validateBeforeSubmit = () => {
+  if (formData.user_password !== formData.user_confirm_password) {
+    toast.error('Passwords do not match.');
+    return false;
+  }
+  return true;
+};
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    if (!validateBeforeSubmit()) {
+      // toast.error('Please fix the validation errors before submitting.');
+      return;
+    }
+
+    const data = new FormData();
+    Object.entries(formData).forEach(([key, value]) => {
+      data.append(key, value);
+    });
+
+    toast.promise(
+      ApiService.post('/user/registration/', data,{
+        headers: {'Content-Type': 'multipart/form-data'}
+      }),
+      {
+        loading: 'Registering...',
+        success: () => {
+          navigate('/user-login');
+          return 'Registration successfully!';
+        },
+        error: (error) => {
+          let msg = "";
+          console.log(error);
+          if (error.response?.status === 400) {
+            if (error.response.data.user_email) {
+              msg = "Email Already Exist!";
+            }
+            if (error.response.data.user_mobile_no) {
+              msg = "Mobile No Already Exist!";
+            }
+            toast.error(msg || 'Invalid data provided.');
+          }
+        },
+      }
+    );
+  }
+
+
   return (
     <div>
       <h1>User Registration</h1>
+      <form onSubmit={handleSubmit}>
+        <label htmlFor="user_name">Name:</label>
+        <input type="text" name="user_name" value={formData.user_name} onChange={handleChange} required />
+
+        <label htmlFor="user_email">Email:</label>
+        <input type="email" name="user_email" value={formData.user_email} onChange={handleChange} required />
+
+        <label htmlFor="user_mobile_no">Mobile No:</label>
+        <input type="tel" name="user_mobile_no" value={formData.user_mobile_no} onChange={handleChange} required />
+
+        <label htmlFor="user_password">Password:</label>
+        <input type="password" name="user_password" value={formData.user_password} onChange={handleChange}
+        minLength={8} maxLength={30} required />
+
+        <label htmlFor="user_confirm_password">Confirm Password:</label>
+        <input type="password" name="user_confirm_password" value={formData.user_confirm_password} onChange={handleChange} minLength={8} maxLength={30} required />
+
+        <button type='submit'>Register</button>
+
+      </form>
+  
     </div>
   )
 }
