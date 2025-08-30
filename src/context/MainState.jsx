85199ccc83839ab5c3b36da2ad274e56d60cac53
Deepak Raikwar <raikwardeepak310@gmail.com>
2025-08-31 03:30:00 +0530

    raideepak07 31-08-2025 03:29

--- src/context/MainState.jsx
+++ src/context/MainState.jsx
@@ -1,12 +1,27 @@
-import  { useState } from 'react'
+import  { useState, useEffect } from 'react'
 import { MainContext } from './Context';
+import ApiService from '../apiservice/ApiService';
 
 const MainState = (props) => {
 
   const [mainData, setMainData] = useState('Deepak')
+  const [locations, setLocations] = useState([])
+
+  const getLocations = async () => {
+    try {
+      const response = await ApiService.get('states-cities/')
+      setLocations(response.data)
+    } catch (error) {
+      console.error('Error fetching locations:', error)
+    }
+  }
+
+  useEffect(() => {
+    getLocations();
+  }, []);
 
   return (
-    <MainContext.Provider value={{ mainData, setMainData }}>
+    <MainContext.Provider value={{ mainData, setMainData, locations }}>
       {props.children}
     </MainContext.Provider>
   )
